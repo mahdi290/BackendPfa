dacc6c797396d68c374bdcff02b270c5dacdfbae
mahdi290 <ghribimahdi8@gmail.com>
2024-03-24 04:32:07 +0100

    hey

--- dockerfile
+++ dockerfile
@@ -1,14 +1,12 @@
-# Use a lightweight base image for Java application runtime
-FROM openjdk:21-slim
+FROM openjdk:21
 
-# Set the working directory in the container
+
 WORKDIR /app
 
-# Copy the JAR file into the container
-COPY target/Services-0.0.1-SNAPSHOT.jar ./app.jar
+COPY target/employeemanager2-0.0.1-SNAPSHOT.jar ./app.jar
 
-# Expose the port your application runs on (if needed)
+
 EXPOSE 8080
 
-# Command to run the application when the container starts
+
 CMD ["java", "-jar", "app.jar"]
--- src/main/java/tech/getarrays/employeemanager/EmployeemanagerApplication.java
+++ src/main/java/tech/getarrays/employeemanager/EmployeemanagerApplication.java
@@ -20,7 +20,7 @@ public class EmployeemanagerApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://0.0.0.0:4200"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
