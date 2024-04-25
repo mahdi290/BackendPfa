2d47e98c5dd82cff2232614d3a0d945b11ec5a22
mahdi290 <83859113+mahdi290@users.noreply.github.com>
2024-04-25 12:47:51 +0200

    Update EmployeemanagerApplication.java

--- src/main/java/tech/getarrays/employeemanager/EmployeemanagerApplication.java
+++ src/main/java/tech/getarrays/employeemanager/EmployeemanagerApplication.java
@@ -2,12 +2,35 @@ package tech.getarrays.employeemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class EmployeemanagerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EmployeemanagerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemanagerApplication.class, args);
+	}
 
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://54.237.226.53:4200")); // Utilisez le nom du service frontend
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		corsConfiguration.setMaxAge(3600L); // set max age to 1 hour
+
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
+	}
 }
