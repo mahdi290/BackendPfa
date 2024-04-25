2c921f5aae5902a5643b663e1b65d4afb15d99db
mahdi290 <83859113+mahdi290@users.noreply.github.com>
2024-04-25 10:48:54 +0200

    Update EmployeemanagerApplication.java

--- src/main/java/tech/getarrays/employeemanager/EmployeemanagerApplication.java
+++ src/main/java/tech/getarrays/employeemanager/EmployeemanagerApplication.java
@@ -18,7 +18,7 @@ public class EmployeemanagerApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://0.0.0.0:4200")); // Allow requests from all origins
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://10.110.73.222:4200")); // Allow requests from all origins
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
